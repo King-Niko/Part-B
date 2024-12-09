feb024e991aed76ee8bf77e9bf36d66fc6ac90f2
King-Niko <nickd_walling!@yahoo.com>
2024-12-08 21:39:25 -0500

    I wrote the templated functions. Now it's time to program the driver.

--- Part B/Part B.cpp
+++ Part B/Part B.cpp
@@ -8,6 +8,20 @@
 #include <cmath>
 using namespace std;
 
+// Template function for floating-point types (float, double)
+template <typename T>
+T half(T value)
+{
+    return (value / 2);
+}
+
+// Specialized template function for integers
+template <>
+int half<int>(int value)
+{
+    return static_cast<int>(round(value / 2.0));
+}
+
 int main()
 {
     // Driver ... test ... Git commit
