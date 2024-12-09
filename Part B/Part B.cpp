905f290ad6b8302163d0e99c8b1cb429c8656688
King-Niko <nickd_walling!@yahoo.com>
2024-12-08 21:27:20 -0500

    Add project files.

---
+++ Part B/Part B.cpp
@@ -0,0 +1,14 @@
+// Nicholas Walling
+// CIS 1202 101
+// December 8, 2024
+// Exceptions, Templates & STL
+// A program that has custom templated functions that perform math on varying numeric data types.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
