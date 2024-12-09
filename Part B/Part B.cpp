ef070f4ded3519051cddfe38681fbf5e87624893
King-Niko <nickd_walling!@yahoo.com>
2024-12-08 21:42:23 -0500

    Added the test variable in the driver.

--- Part B/Part B.cpp
+++ Part B/Part B.cpp
@@ -24,5 +24,11 @@ int half<int>(int value)
 
 int main()
 {
-    // Driver ... test ... Git commit
+    // Test cases for the function
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    // Display the output for each case
+
 }
